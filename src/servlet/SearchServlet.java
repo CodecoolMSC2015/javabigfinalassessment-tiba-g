d962ba66f33cf1ea888f45824e23ce4b260a8330
Tiba Gabor <tibag7@gmail.com>
2016-03-16 16:17:51 +0100

    SearchServer edited

--- src/servlet/SearchServlet.java
+++ src/servlet/SearchServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +14,11 @@ public class SearchServlet extends HttpServlet
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
-		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String searchType = request.getParameter("SearchType");
+		String Criterial = request.getParameter("Criterial");
+		out.write(searchType + " " + Criterial);
 	}
 
 }
